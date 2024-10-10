f535429194aad0888d2e58f518584a79c6c2b1a5
ItzVakko <itzvakkoofficial@gmail.com>
2024-10-10 20:43:50 +0400

    finished navbar

--- app/Components/Header/NavBar.js
+++ app/Components/Header/NavBar.js
@@ -10,17 +10,45 @@ const NavBar = () => {
   const scrollY = UseScrollPosition();
   const [rotateAngle, setRotateAngle] = useState(0);
   const [prevScrollY, setPrevScrollY] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const scrollDiff = scrollY - prevScrollY;
     setRotateAngle((prevAngle) => prevAngle + scrollDiff * 0.07);
     setPrevScrollY(scrollY);
+
+    // Navbar will appear when user scroll down
+    if (scrollY > 0) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
   }, [scrollY, prevScrollY]);
 
   return (
-    <div className="w-full h-20 bg-customGray fixed top-0 left-0 z-50 border-b border-white border-solid">
+    <motion.div
+      className="w-full h-20 bg-customGray px-[120px] fixed top-0 left-0 z-50 border-b border-white border-solid"
+      initial={{ y: -100 }}
+      animate={{ y: isVisible ? 0 : -100 }}
+      transition={{ type: "spring", stiffness: 40 }}
+    >
       <div className="w-full h-full flex justify-between items-center">
-        <div></div>
+        <div className="max-w-[330px]">
+          <ul className="text-white flex gap-10 font-teko text-xl nav-links">
+            <li>
+              <a href="#">About Me</a>
+            </li>
+            <li>
+              <a>Skills</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <a>Education</a>
+            </li>
+          </ul>
+        </div>
 
         <div className="w-28 h-28 transform -translate-y-10">
           <motion.div
@@ -36,9 +64,24 @@ const NavBar = () => {
           </motion.div>
         </div>
 
-        <div></div>
+        <div className="max-w-[330px]">
+          <ul className="text-white flex gap-10 font-teko text-xl nav-links">
+            <li>
+              <a>Facebook</a>
+            </li>
+            <li>
+              <a>Instagram</a>
+            </li>
+            <li>
+              <a>Linkedin</a>
+            </li>
+            <li>
+              <a>Github</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
