e481a8bd98566ad03897b8750542531423fd25bc
ItzVakko <itzvakkoofficial@gmail.com>
2025-03-13 17:39:30 +0400

    added messenger chatbot

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BackgroundImage from "./components/BackgroundImage";
 import HomePage from "./pages/Home/Home";
+import { FacebookProvider, CustomChat } from "react-facebook";
 
 export default function Home() {
   return (
@@ -9,6 +10,10 @@ export default function Home() {
 
       <HomePage />
       <div className="h-[900px]"></div>
+
+      <FacebookProvider appId="2126083514486740" chatSupport>
+        <CustomChat pageId="576324402235215" minimized="true" />
+      </FacebookProvider>
     </>
   );
 }
